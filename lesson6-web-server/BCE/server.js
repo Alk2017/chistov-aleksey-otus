16a4fbabe510d67b555c492994e46497378a929c
a.chistov <a.chistov@vk.team>
2025-06-13 21:44:50 +0300

    -add course view page and websocket comment update there

--- lesson6-web-server/BCE/server.js
+++ lesson6-web-server/BCE/server.js
@@ -7,6 +7,7 @@
 var app = require('./app');
 var debug = require('debug')('bce:server');
 var http = require('http');
+const WebSocket = require('ws');
 
 /**
  * Get port from environment and store in Express.
@@ -88,3 +89,49 @@ function onListening() {
     : 'port ' + addr.port;
   debug('Listening on ' + bind);
 }
+
+
+// Создаем WebSocket-сервер на порту 8080
+const wsserver = new WebSocket.Server({ port: 3010 });
+
+console.log('WebSocket сервер запущен на ws://localhost:3010');
+
+sockets = []
+
+// Обработка подключения клиента
+wsserver.on('connection', (socket) => {
+  console.log('Клиент подключен');
+  sockets.push(socket);
+
+  socket.on('upgrade', function upgrade(request, socket, head) {
+    socket.on('error', onSocketError);
+
+    console.log(request.headers);
+  });
+
+  // Отправляем приветственное сообщение клиенту
+  socket.send('Добро пожаловать на WebSocket сервер!');
+
+  // любая логика нотификацию
+
+  // Обработка входящих сообщений от клиента
+  socket.on('message', (message) => {
+    console.log(`Получено сообщение: ${message}`);
+    // Отправляем ответ клиенту
+    socket.send(`Эхо: ${message}`);
+    sockets.filter(client => client !== socket).forEach(client => {
+      client.send(message.toString());
+    });
+  });
+
+  // Обработка отключения клиента
+  socket.on('close', () => {
+    sockets = sockets.filter(client => client !== socket);
+    console.log('Клиент отключился');
+  });
+
+  // Обработка ошибок
+  socket.on('error', (error) => {
+    console.error('Ошибка WebSocket:', error);
+  });
+});
